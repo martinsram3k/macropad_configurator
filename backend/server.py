8d1f570f8b71a846739261f2af5e0ea86bdc6783
martinsram3k <79270832+martinsram3k@users.noreply.github.com>
2025-03-11 22:18:51 +0100

    firmware pro macropad

--- backend/server.py
+++ backend/server.py
@@ -1,22 +1,19 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+import serial
 
 app = Flask(__name__)
-CORS(app)  # Povolení CORS pro všechny zdroje
+CORS(app)
+
+# Globální proměnná pro sériový port
+ser = None
+
+def init_serial():
+    global ser
+    try:
+        ser = serial.Serial('COM6', 115200)  # Nahraďte 'COM3' správným portem
+        print("Sériový port inicializován")
+    except serial.SerialException as e:
+        print(f"Chyba při inicializaci sériového portu: {e}")
 
 @app.route('/receivedata', methods=['POST'])
-def receive_data():
-    data = request.get_json()
-    print(f"Přijatá data (celá): {data}")  # Přidáno logování celých dat
-    if data:
-        button = data.get('button')
-        layer = data.get('layer')
-        function = data.get('function')
-
-        print(f"Button={button}, Layer={layer}, Function={function}")
-        return jsonify({'message': 'Data byla úspěšně přijata.'}), 200
-    else:
-        return jsonify({'error': 'Žádná data nebyla přijata.'}), 400
-
-if __name__ == '__main__':
-    app.run(port=5000, debug=True)
